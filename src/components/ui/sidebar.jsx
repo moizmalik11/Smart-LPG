1733c366dc8aba8bcbff747e20bb4239dc8b78e5
Moiz Malik <moiz.malik1113@gmail.com>
2026-03-01 23:54:48 +0500

    new cards added

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -100,14 +100,7 @@ const SidebarProvider = React.forwardRef((
   // This makes it easier to style the sidebar with Tailwind classes.
   const state = open ? "expanded" : "collapsed"
 
-  const contextValue = React.useMemo(() => ({
-    state,
-    open,
-    setOpen,
-    isMobile,
-    openMobile,
-    setOpenMobile,
-    toggleSidebar,
+ 
   }), [state, open, setOpen, isMobile, openMobile, setOpenMobile, toggleSidebar])
 
   return (
